a548c5081ebf3ac1363d9cd54db0b1ce4382c5c8
Дмитрий Середа <q3dm0@yahoo.com>
2017-06-28 11:09:10 +0300

    Добавлен медиаметрикс яндекс

--- templates/yug1/index.php
+++ templates/yug1/index.php
@@ -156,5 +156,17 @@ artxComponentWrapper($document);
         $("#toTop").scrollToTop();
     });
 </script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-35115627-3', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 </body>
 </html>
